e19aa5a78fbd3a74d181ab204974c8164ae1c611
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-16 12:21:17 -0700

    Add Material icons for V2 navigation UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.9")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
