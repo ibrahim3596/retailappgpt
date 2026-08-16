1e5aed44529ec80f762412fab380a914c360f22c
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-16 12:30:41 -0700

    Fix Material3 experimental API usage in V2 navigation

--- app/src/main/java/com/retailpos/app/MainActivity.kt
+++ app/src/main/java/com/retailpos/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -41,6 +42,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun RetailPosApp() {
     val navController = rememberNavController()
@@ -79,6 +81,7 @@ private fun RetailPosApp() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun FoundationPlaceholder(title: String, subtitle: String) {
     Scaffold(
