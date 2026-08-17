d21f9a7080054c04176ad82fdc2147d3669dcefc
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-16 23:35:59 -0700

    Fix ReceiptScreen Material3 opt-in

--- app/src/main/java/com/retailpos/app/ui/screens/ReceiptScreen.kt
+++ app/src/main/java/com/retailpos/app/ui/screens/ReceiptScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +20,7 @@ import com.retailpos.app.data.ReceiptFormatter
 import com.retailpos.app.data.SaleEntity
 import com.retailpos.app.data.SaleLineEntity
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReceiptScreen(
     sale: SaleEntity,
