fe37916332d6a00ebbff262ed355f3764e5992a9
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-18 23:02:14 -0700

    Test GTIN-shaped barcode validation

---
+++ app/src/test/java/com/example/retailpos/domain/model/ProductIdentifierValidatorTest.kt
@@ -0,0 +1,48 @@
+package com.example.retailpos.domain.model
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ProductIdentifierValidatorTest {
+    @Test
+    fun validGtinsAreAccepted() {
+        assertTrue(ProductIdentifierValidator.isValidGtIn("4006381333931"))
+        assertTrue(ProductIdentifierValidator.isValidGtIn("036000291452"))
+    }
+
+    @Test
+    fun invalidCheckDigitIsRejected() {
+        assertFalse(ProductIdentifierValidator.isValidGtIn("4006381333932"))
+    }
+
+    @Test
+    fun unsupportedLengthIsRejected() {
+        assertFalse(ProductIdentifierValidator.isValidGtIn("123456789"))
+    }
+
+    @Test
+    fun normalizationRemovesWhitespaceAndNormalizesCase() {
+        assertTrue(ProductIdentifierValidator.normalize("  abc-123  ") == "ABC-123")
+    }
+
+    @Test
+    fun retailPosTypesAreLimitedToConsumerGtins() {
+        assertTrue(ProductIdentifierValidator.isRetailPosGtInType(BarcodeType.EAN_13))
+        assertTrue(ProductIdentifierValidator.isRetailPosGtInType(BarcodeType.UPC_A))
+        assertFalse(ProductIdentifierValidator.isRetailPosGtInType(BarcodeType.ITF_14))
+        assertFalse(ProductIdentifierValidator.isRetailPosGtInType(BarcodeType.QR_CODE))
+    }
+
+    @Test
+    fun gtinShapedNumericBarcodesRequireValidCheckDigit() {
+        assertTrue(ProductIdentifierValidator.isValidRetailBarcode("4006381333931"))
+        assertFalse(ProductIdentifierValidator.isValidRetailBarcode("4006381333932"))
+    }
+
+    @Test
+    fun customNonGtInBarcodeFormatsRemainAllowed() {
+        assertTrue(ProductIdentifierValidator.isValidRetailBarcode("ABC-128-001"))
+        assertTrue(ProductIdentifierValidator.isValidRetailBarcode("123456789"))
+    }
+}
