0fe92fd66fd2d350c6d7dddb3c26328bf783f18d
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-19 05:13:47 -0700

    feat: register product metadata persistence

--- app/src/main/java/com/retailpos/app/data/RetailDatabase.kt
+++ app/src/main/java/com/retailpos/app/data/RetailDatabase.kt
@@ -6,13 +6,14 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [ProductEntity::class, ProductBarcodeEntity::class, SaleEntity::class, SaleLineEntity::class, InventoryMovementEntity::class, InventoryBatchEntity::class, CustomerEntity::class, CustomerLedgerEntry::class, ProductIdentificationCacheEntity::class],
-    version = 10,
+    entities = [ProductEntity::class, ProductBarcodeEntity::class, ProductMetadataEntity::class, SaleEntity::class, SaleLineEntity::class, InventoryMovementEntity::class, InventoryBatchEntity::class, CustomerEntity::class, CustomerLedgerEntry::class, ProductIdentificationCacheEntity::class],
+    version = 11,
     exportSchema = false
 )
 abstract class RetailDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
     abstract fun productBarcodeDao(): ProductBarcodeDao
+    abstract fun productMetadataDao(): ProductMetadataDao
     abstract fun saleDao(): SaleDao
     abstract fun inventoryDao(): InventoryDao
     abstract fun customerDao(): CustomerDao
@@ -24,7 +25,7 @@ abstract class RetailDatabase : RoomDatabase() {
         @Volatile private var INSTANCE: RetailDatabase? = null
         fun get(context: Context): RetailDatabase = INSTANCE ?: synchronized(this) {
             INSTANCE ?: Room.databaseBuilder(context.applicationContext, RetailDatabase::class.java, "retailpos.db")
-                .addMigrations(DatabaseMigrations.MIGRATION_1_2, DatabaseMigrations.MIGRATION_2_3, DatabaseMigrations.MIGRATION_3_4, DatabaseMigrations.MIGRATION_4_5, DatabaseMigrations.MIGRATION_5_6, DatabaseMigrations.MIGRATION_6_7, DatabaseMigrations.MIGRATION_7_8, DatabaseMigrations.MIGRATION_8_9, DatabaseMigrations.MIGRATION_9_10)
+                .addMigrations(DatabaseMigrations.MIGRATION_1_2, DatabaseMigrations.MIGRATION_2_3, DatabaseMigrations.MIGRATION_3_4, DatabaseMigrations.MIGRATION_4_5, DatabaseMigrations.MIGRATION_5_6, DatabaseMigrations.MIGRATION_6_7, DatabaseMigrations.MIGRATION_7_8, DatabaseMigrations.MIGRATION_8_9, DatabaseMigrations.MIGRATION_9_10, DatabaseMigrations.MIGRATION_10_11)
                 .build().also {
                     INSTANCE = it
                     ProductCatalogLookup.configurePersistentCache(LOCAL_STORE_ID, it.productIdentificationCacheDao())
