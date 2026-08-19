c46ea94b490d59406c5cb338812f72f516f628ec
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-19 03:00:36 -0700

    Register persistent identification cache

--- app/src/main/java/com/retailpos/app/data/RetailDatabase.kt
+++ app/src/main/java/com/retailpos/app/data/RetailDatabase.kt
@@ -6,8 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [ProductEntity::class, ProductBarcodeEntity::class, SaleEntity::class, SaleLineEntity::class, InventoryMovementEntity::class, InventoryBatchEntity::class, CustomerEntity::class, CustomerLedgerEntry::class],
-    version = 9,
+    entities = [ProductEntity::class, ProductBarcodeEntity::class, SaleEntity::class, SaleLineEntity::class, InventoryMovementEntity::class, InventoryBatchEntity::class, CustomerEntity::class, CustomerLedgerEntry::class, ProductIdentificationCacheEntity::class],
+    version = 10,
     exportSchema = false
 )
 abstract class RetailDatabase : RoomDatabase() {
@@ -17,12 +17,13 @@ abstract class RetailDatabase : RoomDatabase() {
     abstract fun inventoryDao(): InventoryDao
     abstract fun customerDao(): CustomerDao
     abstract fun khataDao(): KhataDao
+    abstract fun productIdentificationCacheDao(): ProductIdentificationCacheDao
 
     companion object {
         @Volatile private var INSTANCE: RetailDatabase? = null
         fun get(context: Context): RetailDatabase = INSTANCE ?: synchronized(this) {
             INSTANCE ?: Room.databaseBuilder(context.applicationContext, RetailDatabase::class.java, "retailpos.db")
-                .addMigrations(DatabaseMigrations.MIGRATION_1_2, DatabaseMigrations.MIGRATION_2_3, DatabaseMigrations.MIGRATION_3_4, DatabaseMigrations.MIGRATION_4_5, DatabaseMigrations.MIGRATION_5_6, DatabaseMigrations.MIGRATION_6_7, DatabaseMigrations.MIGRATION_7_8, DatabaseMigrations.MIGRATION_8_9)
+                .addMigrations(DatabaseMigrations.MIGRATION_1_2, DatabaseMigrations.MIGRATION_2_3, DatabaseMigrations.MIGRATION_3_4, DatabaseMigrations.MIGRATION_4_5, DatabaseMigrations.MIGRATION_5_6, DatabaseMigrations.MIGRATION_6_7, DatabaseMigrations.MIGRATION_7_8, DatabaseMigrations.MIGRATION_8_9, DatabaseMigrations.MIGRATION_9_10)
                 .build().also { INSTANCE = it }
         }
     }
