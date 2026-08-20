8ca402849bdf28cff0fcd81786b61e08ef826e44
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-20 11:34:05 -0700

    test: cover UPI payment URI construction

---
+++ app/src/test/java/com/retailpos/app/core/payment/UpiPaymentIntentTest.kt
@@ -0,0 +1,19 @@
+package com.retailpos.app.core.payment
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UpiPaymentIntentTest {
+    @Test
+    fun `builds encoded INR UPI uri`() {
+        val uri = UpiPaymentIntent.build("shop name@upi", "My Shop", 125.5, "ref-1")
+        assertEquals("upi", uri.scheme)
+        assertEquals("pay", uri.host)
+        assertTrue(uri.getQueryParameter("pa").contains("shop name@upi"))
+        assertEquals("My Shop", uri.getQueryParameter("pn"))
+        assertEquals("125.50", uri.getQueryParameter("am"))
+        assertEquals("INR", uri.getQueryParameter("cu"))
+        assertEquals("ref-1", uri.getQueryParameter("tr"))
+    }
+}
