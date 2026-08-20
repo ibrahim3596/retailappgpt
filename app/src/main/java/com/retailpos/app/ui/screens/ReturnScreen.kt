527a553863d71af2fe58a68aa93be299dfba6411
ibrahim3596 <ibrahimbusiness3569@gmail.com>
2026-08-20 09:15:39 -0700

    fix: remove stale return screen imports

--- app/src/main/java/com/retailpos/app/ui/screens/ReturnScreen.kt
+++ app/src/main/java/com/retailpos/app/ui/screens/ReturnScreen.kt
@@ -3,7 +3,6 @@ package com.retailpos.app.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -29,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -38,10 +38,7 @@ import com.retailpos.app.data.ReturnCandidateLine
 import com.retailpos.app.data.ReturnRepository
 import com.retailpos.app.data.RetailDatabase
 import com.retailpos.app.data.SaleEntity
-import com.retailpos.app.data.SaleLineEntity
-import com.retailpos.app.data.StaffSessionManager
 import kotlinx.coroutines.launch
-import androidx.compose.runtime.rememberCoroutineScope
 import java.util.Locale
 
 @Composable
